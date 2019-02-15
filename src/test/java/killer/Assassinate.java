6cb1e5621962de6310ade1719f6ef4ac5e173e58
Joshua <joshualpitts@gmail.com>
2019-02-14 20:03:06 -0500

    update for Jenkins:

--- src/test/java/killer/Assassinate.java
+++ src/test/java/killer/Assassinate.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.io.TemporaryFilesystem;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Assassinate {
@@ -19,6 +20,7 @@ public class Assassinate {
 	}
 
 	@BeforeMethod(alwaysRun = true)
+	@Parameters({"isHeadless"})
 	public void beforeMethod() throws Exception {
 //	    String customProfile = System.getProperty("customFirefoxProfile");
 //	    FirefoxProfile profile = new FirefoxProfile(new File(customProfile));
